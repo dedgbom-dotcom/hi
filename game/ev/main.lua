db22ad85d01c95b2020fb07b470a7f015c56eb8d
dedgbom-dotcom <dedgbom@gmail.com>
2026-03-01 23:06:01 +0700

    Refactor WindUI loading and update thumbnail
    Removed old WindUI loading method and updated thumbnail image.

--- game/ev/main.lua
+++ game/ev/main.lua
@@ -1,4 +1,11 @@
 -- ========================================================================== --
+--                              LOAD WINDUI                                    --
+-- ========================================================================== --
+
+local WindUI = getgenv().WindUI
+WindUI:SetTheme("Violet")
+
+-- ========================================================================== --
 --                            SERVICES & MODULES                               --
 -- ========================================================================== --
 
@@ -3678,12 +3685,6 @@ function ESP_System:Stop()
     self:ClearTracerAll() 
 end
 
--- ========================================================================== --
---                              LOAD WINDUI                                    --
--- ========================================================================== --
-
-local WindUI = loadstring(game:HttpGet("https://raw.githubusercontent.com/Footagesus/WindUI/main/dist/main.lua"))()
-WindUI:SetTheme("Violet")
 
 -- ========================================================================== --
 --                              CREATE WINDOW                                  --
@@ -3750,7 +3751,7 @@ local WindowConfigTab = Tabs.Customise:Tab({ Title = "Window Config", Icon = "se
 CredTab:Paragraph({
     Title = "Credits",
     Desc = "Original rzprivate \nGUI Library: WindUI\nVisual by: iruz",
-    Thumbnail = "https://wallpapers.com/images/high/anime-school-girl-cute-short-hair-voydje3wlb8co29h.webp",
+    Thumbnail = "https://wallpapers.com/images/high/animated-girl-arrow-c3cmqx47mo7472j1.webp",
     ThumbnailSize = 150
 })
 
